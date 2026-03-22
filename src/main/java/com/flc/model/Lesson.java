62eeaf0cf74c16762a2593b16eb30d234ef471a5
Sandesh Karki <dashysandesh@gmail.com>
2026-03-22 13:50:21 +0545

    Javadoc signature

--- src/main/java/com/flc/model/Lesson.java
+++ src/main/java/com/flc/model/Lesson.java
@@ -1,3 +1,18 @@
+/**
+ * Furzefield Leisure Centre (FLC) - Group Exercise Booking Management System
+ *
+ * A self-contained desktop application for managing group exercise bookings
+ * across an 8-weekend season. Runs entirely locally - no server, no login,
+ * no internet connection required.
+ *
+ * Module: 7COM1025 - University of Hertfordshire - Season 2025/26
+ *
+ * @author  Sandesh Karki (Dantesrc-sys)
+ * @email   dashysandesh@gmail.com
+ * @version 1.0
+ * @since   2026
+ */
+
 package com.flc.model;
 
 import java.util.ArrayList;
